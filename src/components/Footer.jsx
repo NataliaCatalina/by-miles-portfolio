b49a7fb91dccdb968eabaee4a8ed25e6fe41130f
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-01-18 18:09:04 +1300

    add nav and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 function Footer () {
   return (
     <footer>
-      <div className='container-frooter'>
-        <a href="https://www.linkedin.com/in/miles-wilson-7aa2b170/" target="_blank" rel='noreferrer'><i className="fab fa-linkedin-in"></i></a>
-        <a href="https://vimeo.com/mileswilson"><i className="fab fa-vimeo-v"></i></a>
-      </div>
-      <p>@ 2022 By NataliaCatalina</p>
+      <p>© 2022 Developed by &nbsp;<a href='https://www.nataliacatalina.com/' target="_blank" rel='noreferrer'>Natalia Catalina</a></p>
     </footer>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,17 @@ function Navbar () {
   return (
     <nav>
       
-      <ul>
+      <ul className='navbar'>
         <li>
-          <Link to='/'>BY MILES</Link>
-        </li>
-        <li>
-          <Link to='/animation'>Animation</Link>
-        </li>
-        <li>
-          <Link to='/film'>Film</Link>
+          <Link className='nav-logo' to='/'>BY MILES</Link>
         </li>
         <li>
           <Link to='/about'>About/Contact</Link>
+
+            <a href='https://vimeo.com/mileswilson' target="_blank" rel='noreferrer'><i className="fab fa-vimeo-v"></i></a>
+            <a href='https://www.linkedin.com/in/miles-wilson-7aa2b170/' target="_blank" rel='noreferrer'><i className="fab fa-linkedin-in"></i></a>
+            <a href='https://www.instagram.com/bymiles_nz/' target="_blank" rel='noreferrer'><i className="fab fa-instagram"></i></a>
+
         </li>
       </ul>
       
