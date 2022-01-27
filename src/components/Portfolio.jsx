4f19a52036b7548704b3d9be3dafb85227dcba9d
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-01-27 23:14:47 +1300

    mapping information

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,37 +1,50 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 // DATA 
-import { Projects } from '../data/data'
+import { projects } from '../data/data'
 
 
 function Portfolio() {
+  const [ popContent, setPopContent ] = useState([])
+  const changeContent = (project) => {
+    setPopContent([project])
+  }
   return (
     <>
       <div className="portfolio_container">
         <div className="content_container">
-          {Projects.map(({ id, title, image }) => {
+          {projects.map(( project ) => {
             return (
-              <div key={id}>
-                <Link className="content_card" to={`${id}`}>
+              <div key={project.id}>
+                <a className="content_card" onClick={()=> changeContent(project)}>
                   <img
                     className="card-images"
-                    src={`../images/${image}`}
+                    src={`../images/${project.image}`}
                     alt=""
                   />
-                  <p className="portfolio_text">{title}</p>
-                  <button>Detail</button>
-                </Link>
+                  <p className="portfolio_text">{project.title}</p>
+                </a>
               </div>
             );
           })}
         </div>
         <div className="pop_up_container">
-          <div className="pop_upheader">
+          <div className="pop_up_header">
             <button>X</button>
           </div>
           <div className="pop_up_content">
-            
+            {popContent.map((pop)=>{
+              return (
+                <div key={pop}> 
+                  <div className="pop_up_card" >
+                    <p>test</p>
+                    <p>name: {pop.title} </p>
+                    <p>category: {pop.category}</p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
