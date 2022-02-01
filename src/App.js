f556a7bf05ad1c59b0a88683780a5e4534d72e39
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-02-01 20:23:06 +1300

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-// import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 // DATABASE 
 // COMPONENTS 
 import Navbar from './components/Navbar'
 import Portfolio from './components/Portfolio'
+import About from './components/About'
 import Footer from './components/Footer'
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
     <div className="App">
       <Navbar />
       <div className='app-container'>
-        <Portfolio />
+        <Routes >
+          <Route path='/' element={<Portfolio />} />  
+          <Route path='/about' element={<About />} />  
+        </Routes>
       </div>
       <Footer />
     </div>
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About () {
+  return (
+    <>
+    <h1>Hola soy Miles</h1>
+    </>
+  )
+}
+
+export default About;
