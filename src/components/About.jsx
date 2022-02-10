3aa5713a1f290672572b420fec870865431f73fe
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-02-10 18:28:13 +1300

    working on about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 function About () {
   return (
-    <>
-    <h1>Hola soy Miles</h1>
-    </>
+
+    <div className='about_container'>
+      <div className='image_container'>
+        <img className='about-image' src='../images/MilesWilson.jpg' alt="Black and white portrait of Miles Wilson" />
+      </div>
+      <div className='information_container'>
+        <h1>Contact Me</h1>
+        <div className='line'></div>
+        <p>
+          I'm based in Wellington, NZ, and call the suburb of Brooklyn home. Always keen on a coffee and a chat!
+        </p>
+        <p>
+          Feel free to drop me a line, or flick out an email.
+        </p>
+        <p>Email: <a href = "mailto: miles.dt.wilson@gmail.com">miles.dt.wilson@gmail.com</a></p>
+        <p>Tel: <a href="tel:+6421 126 2046">+6421 126 2046</a></p>
+      </div>
+    </div>
+
   )
 }
 
