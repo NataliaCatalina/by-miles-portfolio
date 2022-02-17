4491ca6cbc82bf1584cfe731b41444595ab7c442
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-02-17 17:07:29 +1300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,6 @@ import { Link } from 'react-router-dom'
 function Navbar () {
   const [ showLinks, setShowLinks ] = useState(false)
   return (
-    // <nav>      
-    //   <ul className='navbar'>
-    //     <li>
-    //       <Link className='nav-logo' to='/'>BY MILES</Link>
-    //     </li>
-    //     <li>
-    //       <Link to='/about'>About/Contact</Link>
-    //         <a href='https://vimeo.com/mileswilson' target="_blank" rel='noreferrer'><i className="fab fa-vimeo-v"></i></a>
-    //         <a href='https://www.linkedin.com/in/miles-wilson-7aa2b170/' target="_blank" rel='noreferrer'><i className="fab fa-linkedin-in"></i></a>
-    //         <a href='https://www.instagram.com/bymiles_nz/' target="_blank" rel='noreferrer'><i className="fab fa-instagram"></i></a>
-    //     </li>
-    //   </ul>
-    // </nav>
-
     <nav>
       <div className="nav-center">
         <div className="nav-header">
