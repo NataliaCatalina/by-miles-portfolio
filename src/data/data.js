fcd4218c7c0617d955291ba9f30d58bbfa667152
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-03-07 16:28:48 +1300

    fixing images on deploy

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ const projects = [
     category: 'Animation',
     categoryClass: 'portfolio-category-animation',
     title: '25 April - Showreel 2016',
-    image: '25-april.JPG',
+    image: 'images/25-april.JPG',
     embed:
       '<iframe class="video" src="https://player.vimeo.com/video/187122203?h=fa80245ddb&autoplay=1&title=0&byline=0&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
       '<p class="video-title"><a href="https://vimeo.com/187122203">Miles Wilson - &quot;25 April&quot; Showreel 2016</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>' +
@@ -15,7 +15,7 @@ const projects = [
     category: 'Animation',
     categoryClass: 'portfolio-category-animation',
     title: 'Sindbad - Showreel 2016',
-    image: 'sindbad.JPG',
+    image: 'images/sindbad.JPG',
     embed:
       '<iframe class="video" src="https://player.vimeo.com/video/181881699?h=bb05db26cc&autoplay=1&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
       '<p class="video-title"><a href="https://vimeo.com/181881699">Miles Wilson - Sindbad Showreel 2016</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>' +
@@ -26,7 +26,7 @@ const projects = [
     category: 'Films',
     categoryClass: 'portfolio-category-film',
     title: 'Faceoff - NZ Short Film',
-    image: 'faceoff.JPG',
+    image: 'images/faceoff.JPG',
     embed:
       '<iframe class="video" width="1000" height="563" src="https://www.youtube.com/embed/ou9tvAK6zgA" title="YouTube video player" frameborder="0" allow="accelerometer; allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
       '<p class="video-title"><a href="https://www.youtube.com/watch?v=ou9tvAK6zgA">Miles Wilson - Faceoff - NZ Short Film</a> from <a href="https://www.youtube.com/channel/UC3YDKrX_LHbR8fAcCfBdg2A">ByMiles Productions</a></p>',
@@ -38,7 +38,7 @@ const projects = [
     category: 'Films',
     categoryClass: 'portfolio-category-film',
     title: 'Off Course - NZ Short Film',
-    image: 'offCourse.JPG',
+    image: 'images/offCourse.JPG',
     embed:
       '<iframe class="video" width="1000" height="563" src="https://www.youtube.com/embed/or8ezRYqJGA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
       '<p class="video-title"><a href="https://www.youtube.com/watch?v=or8ezRYqJGA">Miles Wilson - Off Course - NZ Short Film</a> from <a href="https://www.youtube.com/channel/UC3YDKrX_LHbR8fAcCfBdg2A">ByMiles Productions</a></p>',
@@ -50,7 +50,7 @@ const projects = [
     category: 'Videography',
     categoryClass: 'portfolio-category-videography',
     title: 'Wellington Wolves Training',
-    image: 'wolves-trainingl.jpg',
+    image: 'images/wolves-trainingl.jpg',
     embed: '<iframe class="video" src="https://player.vimeo.com/video/296728763?h=5b20ec0737&autoplay=1&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
     '<p class="video-title"><a href="https://vimeo.com/296728763">Wellington Wolves Training</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>' +
     '<p class="video-description">Natalia and I had the pleasure of photographing this American Football team during training. I made sure to get some extra footage as we worked, and this is what I got.<br />   bymiles.co.nz</p>'
@@ -60,7 +60,7 @@ const projects = [
     category: 'Videography',
     categoryClass: 'portfolio-category-videography',
     title: 'LUX Festival Wellington',
-    image: 'lux.jpg',
+    image: 'images/lux.jpg',
     embed: '<iframe class="video" src="https://player.vimeo.com/video/280911174?h=a71c2b9c60&autoplay=1&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
     '<p class="video-title"><a href="https://vimeo.com/280911174">Miles Wilson - LUX Festival Wellington</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>'
   },
