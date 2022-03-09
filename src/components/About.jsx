0cabab946a00583f7e6fa56e4ab92a7d8cd8a11a
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-03-09 13:39:36 +1300

    figured out photos and deploy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About () {
 
     <div className='about_container'>
       <div className='image_container'>
-        <img className='about-image' src='../images/MilesWilson.jpg' alt="Black and white portrait of Miles Wilson" />
+        <img className='about-image' src={require('../images/miles-wilson.jpg')} alt="Black and white portrait of Miles Wilson" />
       </div>
       <div className='information_container'>
         <h1>Contact Me</h1>
