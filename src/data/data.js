25ac633118583421ac5a11c5c832d0484cb172d9
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-03-09 14:20:16 +1300

    some changes on mobile

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const projects = [
   {
     id: 1,
@@ -68,74 +67,3 @@ const projects = [
 ];
 
 export default projects;
-=======
-const projects = [
-  {
-    id: 1,
-    category: 'Animation',
-    categoryClass: 'portfolio-category-animation',
-    title: '25 April - Showreel 2016',
-    image: '.25april.jpg',
-    embed:
-      '<iframe class="video" src="https://player.vimeo.com/video/187122203?h=fa80245ddb&autoplay=1&title=0&byline=0&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
-      '<p class="video-title"><a href="https://vimeo.com/187122203">Miles Wilson - &quot;25 April&quot; Showreel 2016</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>' +
-      '<p class="video-description">My work on Flux Animation Studio&#039;s &quot;25 April&quot;, a comic-styled documentary of Gallipoli.<br />    I served as a Junior Animator on this film, delivering 3D character animation of feature quality. One of the interesting challenges I faced with this was the animation style, which utilized a low frame rate and an emphasis on static holds to sell the comic book aesthetic.<br />    Credit to Flux Animation Studios for the production.<br />    Music credit to Ladysmith Black Mambazo - &quot;Rain, Rain, Beautiful Rain&quot;<br />    All 3D Character Animation by Miles Wilson</p>'
-  },
-  {
-    id: 2,
-    category: 'Animation',
-    categoryClass: 'portfolio-category-animation',
-    title: 'Sindbad - Showreel 2016',
-    image: 'sindbad.jpg',
-    embed:
-      '<iframe class="video" src="https://player.vimeo.com/video/181881699?h=bb05db26cc&autoplay=1&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
-      '<p class="video-title"><a href="https://vimeo.com/181881699">Miles Wilson - Sindbad Showreel 2016</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>' +
-      '<p class="video-description">A selection of my work on the unreleased TV show, Sindbad and the Seven Galaxies.</p>'
-  },
-  {
-    id: 3,
-    category: 'Films',
-    categoryClass: 'portfolio-category-film',
-    title: 'Faceoff - NZ Short Film',
-    image: 'faceoff.jpg',
-    embed:
-      '<iframe class="video" width="1000" height="563" src="https://www.youtube.com/embed/ou9tvAK6zgA" title="YouTube video player" frameborder="0" allow="accelerometer; allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
-      '<p class="video-title"><a href="https://www.youtube.com/watch?v=ou9tvAK6zgA">Miles Wilson - Faceoff - NZ Short Film</a> from <a href="https://www.youtube.com/channel/UC3YDKrX_LHbR8fAcCfBdg2A">ByMiles Productions</a></p>',
-    description:
-      'Dissatisfied with her life, Helen finds a new path laid out before her. Will she embrace her power, or give up her chance at a different fate?'
-  },
-  {
-    id: 4,
-    category: 'Films',
-    categoryClass: 'portfolio-category-film',
-    title: 'Off Course - NZ Short Film',
-    image: 'offCourse.jpg',
-    embed:
-      '<iframe class="video" width="1000" height="563" src="https://www.youtube.com/embed/or8ezRYqJGA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
-      '<p class="video-title"><a href="https://www.youtube.com/watch?v=or8ezRYqJGA">Miles Wilson - Off Course - NZ Short Film</a> from <a href="https://www.youtube.com/channel/UC3YDKrX_LHbR8fAcCfBdg2A">ByMiles Productions</a></p>',
-    description:
-      'Evie has always seen her father as her opposite, a man lacking her ambitions as an adventurer. However, when she discovers evidence of an exciting old life, nothing can stop her from uncovering the truth behind it, and possibly reconnecting with her brother in the process...'
-  },
-  {
-    id: 5,
-    category: 'Videography',
-    categoryClass: 'portfolio-category-videography',
-    title: 'Wellington Wolves Training',
-    image: 'wolvesTrainingl.jpg',
-    embed: '<iframe class="video" src="https://player.vimeo.com/video/296728763?h=5b20ec0737&autoplay=1&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
-    '<p class="video-title"><a href="https://vimeo.com/296728763">Wellington Wolves Training</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>' +
-    '<p class="video-description">Natalia and I had the pleasure of photographing this American Football team during training. I made sure to get some extra footage as we worked, and this is what I got.<br />   bymiles.co.nz</p>'
-  },
-  {
-    id: 6,
-    category: 'Videography',
-    categoryClass: 'portfolio-category-videography',
-    title: 'LUX Festival Wellington',
-    image: 'lux.jpg',
-    embed: '<iframe class="video" src="https://player.vimeo.com/video/280911174?h=a71c2b9c60&autoplay=1&portrait=0" width="1000" height="563" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
-    '<p class="video-title"><a href="https://vimeo.com/280911174">Miles Wilson - LUX Festival Wellington</a> from <a href="https://vimeo.com/mileswilson">ByMiles Productions</a> on <a href="https://vimeo.com">Vimeo</a>.</p>'
-  },
-];
-
-export default projects;
->>>>>>> 239a9d02c2ff3e1c569d1f3b4e30aedeafbda39e
