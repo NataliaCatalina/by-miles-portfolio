7b60ec66ced701186a3b2322e1bb0b26a635b052
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2022-03-15 20:36:57 +1300

    Updates

---
+++ precache-manifest.48d9ec989d0c824bc5a4b115ea9bcc70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f973538c83b3f95d1fdf",
+    "url": "/static/css/main.af83e8ab.chunk.css"
+  },
+  {
+    "revision": "f973538c83b3f95d1fdf",
+    "url": "/static/js/main.511298d3.chunk.js"
+  },
+  {
+    "revision": "89835fe290a45d3e62a0",
+    "url": "/static/js/runtime~main.ba9f6ff0.js"
+  },
+  {
+    "revision": "36770140c4e513c056a4",
+    "url": "/static/js/2.3d00d10e.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "77d24028e9d296220f2b8e0dc93632a4",
+    "url": "/static/media/miles-wilson.77d24028.jpg"
+  },
+  {
+    "revision": "e9b980af5d3bbd6ea28ad24ca63a5d96",
+    "url": "/static/media/pop-art.e9b980af.png"
+  },
+  {
+    "revision": "3a9aad2bd155108ee7368896f41d23a6",
+    "url": "/static/media/25april.3a9aad2b.jpg"
+  },
+  {
+    "revision": "5f308ae54000c7a0106572a308e9b5c7",
+    "url": "/static/media/face-off.5f308ae5.jpg"
+  },
+  {
+    "revision": "7667f9bc49c2b8b0674c6433711a6589",
+    "url": "/static/media/lux.7667f9bc.jpg"
+  },
+  {
+    "revision": "d60fca801a8b96d4e73645371718ec17",
+    "url": "/static/media/off-course.d60fca80.jpg"
+  },
+  {
+    "revision": "607b091fff52114c57a80bb1394fd3aa",
+    "url": "/static/media/sindbad.607b091f.jpg"
+  },
+  {
+    "revision": "f3dc4b2c7f6111f9aa0d0730e5dfa9cb",
+    "url": "/static/media/wolves-training.f3dc4b2c.jpg"
+  },
+  {
+    "revision": "8201902dac81bb150b82578e513d28d7",
+    "url": "/index.html"
+  }
+];
