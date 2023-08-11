e9d999d497db8c275da03c7ab4da9ba462fd4365
nataliacatalina <ntlc.ramirez@gmail.com>
2023-08-11 14:22:15 +1200

    active category in portfolio submenu

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
+
 function Categories({ categories, filterProjects }) {
+  const [activeCategory, setActiveCategory] = useState(null)
+
+  const handleFilterClick = (category) => {
+    filterProjects(category)
+    setActiveCategory(category)
+  }
+
   return (
     <div className="category-btn-container">
       {categories.map((category, index) => {
         return (
           <button
-            className="filter-btn"
+            className={`filter-btn ${
+              activeCategory === category ? 'active' : ''
+            }`}
             type="button"
             key={index}
-            onClick={() => filterProjects(category)}
+            onClick={() => handleFilterClick(category)}
           >
             {category}
           </button>
