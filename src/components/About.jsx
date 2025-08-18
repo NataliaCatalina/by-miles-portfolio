0d177b2c24e49740909fd26cb4096f8be92e21fd
Natalia Ramirez <ntlc.ramirez@gmail.com>
2025-08-18 14:17:36 +1200

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,16 +15,16 @@ function About() {
         {/* <h1>Contact Me</h1> */}
         <div className="line"></div>
         <p>
-          I'm based in Wellington, NZ, and call the suburb of Brooklyn home.
-          Always keen on a coffee and a chat!
+         As an experienced VFX Artist, 3D Freelancer and Animator, I’ve developed a broad skillset that has allowed me to contribute to a diverse set of projects. I actively maintain positive and collaborative working environments, and am most engaged in my work when it is at its most challenging.
         </p>
-        <p>Feel free to drop me a line, or flick out an email.</p>
+        <p>
+        I have worked on 3D animated films, kids TV shows, tentpole VFX films, and indie shorts. I am experienced in a wide range of software, including Maya, Houdini, Unreal Engine, Katana, Blender, Substance Painter, Nuke, DaVinci Resolve, and many more.
+        </p>
+        <p>Feel free flick out an email.</p>
         <p>
           Email: <a href="mailto: bymiles.nz@gmail.com">bymiles.nz@gmail.com</a>
         </p>
-        <p>
-          Tel: <a href="tel:+6421 126 2046">+6421 126 2046</a>
-        </p>
+
       </div>
     </div>
   )
